a2b231ccc710fb957ca235fe0c0e3261ac940177
Hugh Sun <wenjunsun99@gmail.com>
2023-02-04 18:49:35 -0800

    changed some security constants.

--- starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
+++ starter_code/src/main/java/com/example/demo/security/SecurityConstants.java
@@ -2,8 +2,8 @@ package com.example.demo.security;
 
 public class SecurityConstants {
 
-    public static final String SECRET = "oursecretkey";
-    public static final long EXPIRATION_TIME = 864_000_000; // 10 days
+    public static final String SECRET = "SecretKeyForJWT";
+    public static final long EXPIRATION_TIME = 3600000; // token expires in 1 hour
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/api/user/create";

5c60705ef68bc78594919e97e9f3acdf973bb3ac
Hugh Sun <wenjunsun99@gmail.com>
2023-02-04 19:56:11 -0800

    added logging to UserDetailsServiceImpl

--- starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
+++ starter_code/src/main/java/com/example/demo/security/UserDetailsServiceImpl.java
@@ -2,6 +2,8 @@ package com.example.demo.security;
 
 import java.util.Collections;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,10 +19,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final Logger log = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) {
+            log.error("In UserDetailsServiceImpl::loadUserByUsername, user found is null.");
             throw new UsernameNotFoundException(username);
         }
         return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());

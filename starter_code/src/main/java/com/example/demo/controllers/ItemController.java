1341ee928bd19bb019c9b6e61f83070d4763e35d
Hugh Sun <wenjunsun99@gmail.com>
2023-02-04 22:51:02 -0800

    added log to ItemController

--- starter_code/src/main/java/com/example/demo/controllers/ItemController.java
+++ starter_code/src/main/java/com/example/demo/controllers/ItemController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,19 +20,24 @@ public class ItemController {
 
 	@Autowired
 	private ItemRepository itemRepository;
+
+	private static final Logger log = LoggerFactory.getLogger(ItemController.class);
 	
 	@GetMapping
 	public ResponseEntity<List<Item>> getItems() {
+		log.info("ItemController is trying to get items.");
 		return ResponseEntity.ok(itemRepository.findAll());
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+		log.info("ItemController is trying to get item by id.");
 		return ResponseEntity.of(itemRepository.findById(id));
 	}
 	
 	@GetMapping("/name/{name}")
 	public ResponseEntity<List<Item>> getItemsByName(@PathVariable String name) {
+		log.info("ItemController is trying to get items by name.");
 		List<Item> items = itemRepository.findByName(name);
 		return items == null || items.isEmpty() ? ResponseEntity.notFound().build()
 				: ResponseEntity.ok(items);

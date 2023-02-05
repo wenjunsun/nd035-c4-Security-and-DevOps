5cf9b1bc5a8f2b9d49ad77e2832939776d9c0e63
Hugh Sun <wenjunsun99@gmail.com>
2023-02-04 22:57:54 -0800

    added log to OrderController

--- starter_code/src/main/java/com/example/demo/controllers/OrderController.java
+++ starter_code/src/main/java/com/example/demo/controllers/OrderController.java
@@ -2,6 +2,8 @@ package com.example.demo.controllers;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,25 +29,34 @@ public class OrderController {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+
+	private static final Logger log = LoggerFactory.getLogger(OrderController.class);
 	
 	
 	@PostMapping("/submit/{username}")
 	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		log.info("OrderController is trying to submit an order for " + username);
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("In OrderController::submit, user found is null");
 			return ResponseEntity.notFound().build();
 		}
 		UserOrder order = UserOrder.createFromCart(user.getCart());
 		orderRepository.save(order);
+		log.info("successfully submitted order for " + username);
 		return ResponseEntity.ok(order);
 	}
 	
 	@GetMapping("/history/{username}")
 	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+		log.info("OrderController is trying to get orders for user " + username);
 		User user = userRepository.findByUsername(username);
 		if(user == null) {
+			log.error("In OrderController::getOrdersForUser, user found is null");
 			return ResponseEntity.notFound().build();
 		}
-		return ResponseEntity.ok(orderRepository.findByUser(user));
+		List<UserOrder> userOrders = orderRepository.findByUser(user);
+		log.info("successfully got orders for user " + username);
+		return ResponseEntity.ok(userOrders);
 	}
 }
